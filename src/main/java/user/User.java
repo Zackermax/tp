6766e973c57db7a64c0e69d10c9aedb4f0922d7b
Zackermax <104476724+Zackermax@users.noreply.github.com>
2024-10-09 11:39:50 +0800

    Refactor: Split Gender enum into seperate class from User

--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -1,22 +1,18 @@
 package user;
+import enums.Gender;
 
 public class User {
-
-    public enum Gender {
-        MALE,
-        FEMALE
-    }
 
     public Gender gender;
     public int age;
 
     public User(String gender, String age) {
-        this.gender = User.Gender.valueOf(gender.toUpperCase());
+        this.gender = Gender.valueOf(gender.toUpperCase());
         this.age = Integer.parseInt(age);
     }
 
     public void setGender(String gender) {
-        this.gender = User.Gender.valueOf(gender.toUpperCase());
+        this.gender = Gender.valueOf(gender.toUpperCase());
     }
 
     public void setAge(String age) {
